cfe31b315930ae559c0d9d1410f7a1261c4be388
Ján Vorčák <jan.vorcak@opoint.com>
2016-01-08 23:05:11 +0100

    Redux routing

--- common/store/configureStore.js
+++ common/store/configureStore.js
@@ -1,10 +1,12 @@
 import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
+import createLogger from 'redux-logger'
 import rootReducer from '../reducers'
 
+const logger = createLogger();
 const createStoreWithMiddleware = applyMiddleware(
-  thunk
-)(createStore)
+  thunk, logger
+)(createStore);
 
 export default function configureStore(initialState) {
   const store = createStoreWithMiddleware(rootReducer, initialState)
